e8d677ee1debaf0684e10a25d7ecb63a38fb8679
Lukas Waslowski <cr7pt0gr4ph7@gmail.com>
2024-03-25 15:06:20 +0100

    Improve the board configuration
    Change-Id: Ia81565710e0a9883d7902356d4478193b3208a89

--- lineage_P023.mk
+++ lineage_P023.mk
@@ -1,6 +1,11 @@
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
+# Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+
+# Inherit from P023 device
+$(call inherit-product, device/asus/P023/device.mk)
+
+# Inherit some common Lineage stuff
+$(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
 
 # Boot animation
 TARGET_SCREEN_WIDTH := 1280
